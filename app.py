48f4cefa28b1cc16395819b6989d3b817de15300
bellyk4real <bellyk4real@gmail.com>
2019-08-29 09:55:43 +0100

    added logic for LSTM sentiment analysis model

--- app.py
+++ app.py
@@ -1,5 +1,35 @@
 from flask import Flask
 from flask import request
+from keras.preprocessing import sequence
+from keras.models import load_model
+from keras.preprocessing.text import text_to_word_sequence
+from keras.datasets import imdb
+import numpy as np
+
+# maximum words in each sentence
+maxlen = 10
+# get the word index from imdb dataset
+word_index = imdb.get_word_index()
+# load the Model from file
+nlp_model = load_model('model/imdb_nlp.h5')
+# method that does the prediction – we will call this later
+def predict_sentiment(my_test):
+    # tokenize the sentence
+    word_sequence = text_to_word_sequence(my_test)
+    # create a blank sequence of integers
+    int_sequence = []
+    # for each word in the sentence
+    for w in word_sequence:
+        # get the integer from vocabulary and add to list
+        int_sequence.append(word_index[w])
+        # pad the sequence of numbers to input size expected by model
+        sent_test = sequence.pad_sequences([int_sequence],
+            maxlen=maxlen)
+        # make a prediction using our Model
+        y_pred = nlp_model.predict(sent_test)
+        # return a predicted sentiment real value between 0 and 1
+        return y_pred[0][0]
+
 
 # create the Flask app
 app = Flask(__name__)
